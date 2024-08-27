ee647e70564b920b11f1e42b34225175ba8e5438
SiyangLiu <liusiyang.ocean@gmail.com>
2024-08-27 16:17:57 +0800

    modify example

---
+++ example/bin/step4.plink.kinship.s2.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=06-00:00:00
+#SBATCH --mem=1G
+#SBATCH --job-name=plink_kinship
+#SBATCH --output=plink.kinship.slurm.out
+
+plink2=/share/home/lsy_liusiyang/software/plink2.0/plink2
+imputed_vcf=/share/home/lsy_liusiyang/NIPT-human-genetics/example/outdir/glimpse_output/imputed_file_merged/glimpse.chr20_imputed.vcf.gz
+prefix=`basename $imputed_vcf|sed -e 's/\.vcf\.gz//'`
+kinship_outdir=/share/home/lsy_liusiyang/NIPT-human-genetics/example/outdir/kinship_output
+mkdir -p $kinship_outdir
+
+$plink2 --vcf $imputed_vcf --king-cutoff-table 0.177 --out $kinship_outdir/$prefix --threads 24
