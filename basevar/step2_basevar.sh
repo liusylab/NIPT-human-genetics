8e080682cf6f18a09976cbc2fa69a21b9aeb7145
SiyangLiu <liusiyang.ocean@gmail.com>
2024-09-12 10:27:26 +0800

    update basevar direcotry

--- basevar/step2_basevar.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-basevar basetype -R reference.fasta \
-    -r chr11:5246595-5248428,chr17:41197764-41276135 \
-    -B 200 -t 20 \
-    -L bamfile.list \
-    --output-vcf test.vcf.gz \
-    --output-cvg test.cvg.tsv.gz && echo "** job done **"
