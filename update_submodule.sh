f252d46def20b44e3f72e675bd4fb32255caf53f
Shujia Huang <huangshujia9@gmail.com>
2024-09-20 11:18:08 +0800

    first commit

---
+++ update_submodule.sh
@@ -0,0 +1,13 @@
+# add submodule: basevar 
+git submodule add https://github.com/ShujiaHuang/basevar.git
+
+# init the submodule and fetch all the files of basevar locally
+git submodule update --init --recursive
+
+# update the submodule
+git submodule update --remote
+
+
+
+
+
